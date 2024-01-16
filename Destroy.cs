6c800e2e1f4e6c5a022886d6b48267deb6125d00
nesorok <156800528+nesorok@users.noreply.github.com>
2024-01-16 15:49:41 +0300

    Add files via upload

---
+++ Destroy.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    void Start()
+    {
+        // Команда Destroy(gameObject) уничтожает текущий игровой объект (на котором находится скрипт).
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+
+    }
+}
